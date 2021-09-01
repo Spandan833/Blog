498f2d790a73ccdb0f17d551c1c77214534b33cb
SosukeAizen833 <Spandan833@gmail.com>
2021-09-01 14:53:29 +0530

    new article

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test',(req,res)=>{
-    res.send('In Articles')
+    res.render('articles/new')
 })
 
 module.exports = router;
