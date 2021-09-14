6a20936794b92c0fe5443db7f19eb91cd160f71c
SosukeAizen833 <Spandan833@gmail.com>
2021-09-14 21:18:57 +0530

    fixed method override ugs

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override');
 const app = express();
 
-mongoose.connect('mongodb://localhost/myBlog',{useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex: true});
-app.use(express.urlencoded({extended: false}))
-app.use('/articles',articleRouter)
+mongoose.connect('mongodb://localhost/myBlog',{
+    useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex: true
+});
 
 
 
 
 app.set('view engine','ejs');
-
+app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
+
 app.get('/',async (req,res)=>{
     const articles = await Article.find().sort({
         createdAt: 'desc'
@@ -22,5 +23,7 @@ app.get('/',async (req,res)=>{
 
     res.render("articles/index",{articles: articles})
 })
+app.use('/articles',articleRouter)
+
 
 app.listen(5000);
